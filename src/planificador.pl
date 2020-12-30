dd95dc6e2875eaa769fb9fb2d9ce6b89d55e0e68
borjaguanchesicilia <bg.sicilia@gmail.com>
2020-12-30 17:02:34 +0000

    Recogemos distancia y nueva clasificacion

--- src/planificador.pl
+++ src/planificador.pl
@@ -31,11 +31,11 @@ num_palets([_|L], N) :- num_palets(L, N1), N is N1 + 1.
 
 % Tipo de caminon:
 
-cam(N, X) :- X is 1 -> (N > 1 , N =< 10).
+cam(N, X, D) :- X is 1 -> ((N > 1 , N =< 10) , (D < 100)).
 
-cam(N, X) :- X is 2 -> (N > 11 , N =< 19).
+cam(N, X, D) :- X is 2 -> ((N > 11 , N =< 19) , (D < 150)).
 
-cam(N, X) :- X is 3 -> (N > 20 , N =< 30).
+cam(N, X, D) :- X is 3 -> ((N > 20 , N =< 30) ; (D < 250)).
 
 
 % Busqueda para asegurarnos que el producto del palet existe.
@@ -45,16 +45,18 @@ productos([H|T]) :- productos(T),
 	sensible(H) ; fresco(H) ; nocivo(H) ; empaquetados(H) ; agua(H).
     
     
+distancia([H|T], H, T).
+
     
 planificador(I, L, O) :-
     
-    	productos(L),
+    	distancia(L, D, P),
+    	productos(P),
     	%(not_adjacent(detergentes, frutas, L) ; member(frutas, L)),
     	%(not_adjacent(detergentes, verduras, L) ; member(verduras, L)),
     	%(not_adjacent(detergentes, carne, L) ; member(carne, L)),
     	%(not_adjacent(detergentes, pescado, L); member(pescado, L)),
-	reparto(L, O),
-	num_palets(L, N),
-    	cam(N, X),
-    	append([X], [], Aux),
-    	append([N], Aux, I).
+	reparto(P, O),
+	num_palets(P, N),
+    	cam(N, X, D),
+    	append([], [X, D, N], I).
