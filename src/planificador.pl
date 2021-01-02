031a6918c3c468f19a69e3b7b877a200a840f350
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-01-02 13:46:09 +0000

    SRC: Funciona (mejoramos distancias (BC))

--- src/planificador.pl
+++ src/planificador.pl
@@ -1,4 +1,4 @@
-% Productos que pueden haber:
+%Productos que pueden haber:
 
 sensible(verduras).
 sensible(frutas).
@@ -11,20 +11,20 @@ agua(agua).
 
 % Sedes de los supermercados (base en polígono de Guímar) 
 
-municipio(candelaria, 5).
-municipio(guimar, 2).
-municipio(fasnia, 20).
-municipio(arico, 30).
-municipio(arona, 55).
-municipio(arona, 60).
-municipio(adeje, 70).
-municipio(adeje, 62).
-municipio(santaCruz, 18).
-municipio(laLaguna, 25).
-municipio(puertoCruz, 58).
-municipio(puertoCruz, 56).
-municipio(orotava, 46).
-municipio(realejos, 60).
+municipio(candelaria, D) :- D is 5 + 0.
+municipio(guimar, D) :- D is 2 + 0.
+municipio(fasnia, D) :- D is 20 + 0.
+municipio(arico, D) :- D is 30 + 0.
+municipio(arona, D) :- D is 55 + 0.
+municipio(arona, D) :- D is 60 + 0.
+municipio(adeje, D) :- D is 70 + 0.
+municipio(adeje, D) :- D is 62 + 0.
+municipio(santaCruz, D) :- D is 18 + 0.
+municipio(laLaguna, D) :- D is 25 + 0.
+municipio(puertoCruz, D) :- D is 58 + 0.
+municipio(puertoCruz, D) :- D is 56 + 0.
+municipio(orotava, D) :- D is 46 + 0.
+municipio(realejos, D) :- D is 60 + 0.
 
 
 % Orden de reparto:
@@ -57,7 +57,7 @@ productos([H|T]) :- productos(T),
     
 % Contar kilometros de ir a cada sede desde la base y volver
 sedes([], _, 0).
-sedes([M, D|T], _, Q) :-  municipio(M, D),  sedes(T, D, S), Q is (D * 2) + S. 
+sedes([M|T], _, Q) :-  municipio(M, D),  sedes(T, D, S), Q is (D * 2) + S. 
 
 
 planificador(I, L, S, O) :-
---
+++ src/productos.pl
@@ -0,0 +1,9 @@
+% Productos que pueden haber:
+
+sensible(verduras).
+sensible(frutas).
+fresco(carne).
+fresco(pescado).
+nocivo(detergentes).
+empaquetados(cereales).
+agua(agua).
