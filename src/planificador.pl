0271ef0dc78ebab5a7af0eb5f0a197aedbfcbe72
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-01-04 12:33:36 +0000

    SRC: actualizacion comprobar Adyacente

--- src/planificador.pl
+++ src/planificador.pl
@@ -17,20 +17,20 @@ agua(agua).
 
 % Sedes de los supermercados (base en polígono de Guímar) 
 
-municipio(candelaria, D) :- D is 5 + 0.
-municipio(guimar, D) :- D is 2 + 0.
-municipio(fasnia, D) :- D is 20 + 0.
-municipio(arico, D) :- D is 30 + 0.
-municipio(arona, D) :- D is 55 + 0.
-municipio(arona, D) :- D is 60 + 0.
-municipio(adeje_1, D) :- D is 70 + 0.
-municipio(adeje_2, D) :- D is 62 + 0.
-municipio(santaCruz, D) :- D is 18 + 0.
-municipio(laLaguna, D) :- D is 25 + 0.
-municipio(puertoCruz_1, D) :- D is 58 + 0.
-municipio(puertoCruz_2, D) :- D is 56 + 0.
-municipio(orotava, D) :- D is 46 + 0.
-municipio(realejos, D) :- D is 60 + 0.
+municipio(candelaria, Distancia) :- Distancia is 5 + 0.
+municipio(guimar, Distancia) :- Distancia is 2 + 0.
+municipio(fasnia, Distancia) :- Distancia is 20 + 0.
+municipio(arico, Distancia) :- Distancia is 30 + 0.
+municipio(arona, Distancia) :- Distancia is 55 + 0.
+municipio(arona, Distancia) :- Distancia is 60 + 0.
+municipio(adeje_1, Distancia) :- Distancia is 70 + 0.
+municipio(adeje_2, Distancia) :- Distancia is 62 + 0.
+municipio(santaCruz, Distancia) :- Distancia is 18 + 0.
+municipio(laLaguna, Distancia) :- Distancia is 25 + 0.
+municipio(puertoCruz_1, Distancia) :- Distancia is 58 + 0.
+municipio(puertoCruz_2, Distancia) :- Distancia is 56 + 0.
+municipio(orotava, Distancia) :- Distancia is 46 + 0.
+municipio(realejos, Distancia) :- Distancia is 60 + 0.
 
 
 % Nº de palets:
@@ -41,11 +41,11 @@ num_palets([_|L], N) :- num_palets(L, N1), N is N1 + 1.
 
 % Tipo de caminon:
 
-cam(N, D, X) :- X is 1 -> ((N > 1 , N =< 10) , (D < 100)).
+cam(Palets, Distancia, X) :- X is 1 -> ((Palets > 1 , Palets =< 10) , (Distancia < 100)).
 
-cam(N, D, X) :- X is 2 -> ((N > 11 , N =< 19) , (D < 150)).
+cam(Palets, Distancia, X) :- X is 2 -> ((Palets > 11 , Palets =< 19) , (Distancia < 150)).
 
-cam(N, D, X) :- X is 3 -> ((N > 20 , N =< 30) ; (D < 300)).
+cam(Palets, Distancia, X) :- X is 3 -> ((Palets > 20 , Palets =< 30) ; (Distancia < 300)).
 
 
 % Busqueda para asegurarnos que el producto del palet existe.
@@ -62,49 +62,49 @@ productos([H|T]) :- (sensible(H);
 	  
 % Contar kilometros de ir a cada sede desde la base y volver
 sedes([], 0).
-sedes([M|T], Q) :-  municipio(M, D), sedes(T, S), Q is (D * 2) + S. 
+sedes([SedeMunicipio|T], F) :-  municipio(SedeMunicipio, Distancia), sedes(T, S), F is (Distancia * 2) + S. 
 
 
-palets30(N) :- 30 =< N -> (write('\n\n ALERTA: No se puede llevar mas de 30 palets. \n\n') , exit) ; true .
+palets30(Palets) :- 30 =< Palets -> (write('\n\n ALERTA: No se puede llevar mas de 30 palets. \n\n') , exit) ; true .
 
 
-distancia300(D) :- 300 =< D -> (write('\n\n ALERTA: No se puede recorrer más de 300 km. \n\n') , exit) ; true .
+distancia300(Distancia) :- 300 =< Distancia -> (write('\n\n ALERTA: No se puede recorrer más de 300 km. \n\n') , exit) ; true .
 
 
 exit :- halt.
 
 
-concatena([], Y, Y).
-concatena([H|T1], Y, [H|T2]) :- concatena(T1, Y, T2).
+comprobarAdyacente(X, X).
+comprobarAdyacente(X, [_|T]) :- comprobarAdyacente(X, T).
 
 
-adyacente(X, Y, L) :- 	(concatena(_, [X, Y|_], L) ->  false);
+adyacente(P1, P2, Productos) :- (comprobarAdyacente([P1, P2|_], Productos) ->  false);
 
-			(not(concatena(_, [X, Y|_], L)) -> true).
-
-
-general(I, L, S, Orden) :- permutation(Orden, L), planificador(I, Orden, S).
+				 (not(comprobarAdyacente([P1, P2|_], Productos)) -> true).
 
 
-planificador(I, L, S) :-
-    	productos(L),
-    	adyacente(productoNocivo, frutas, L),
-    	adyacente(frutas, productoNocivo, L),
-    	adyacente(verduras, productoNocivo, L),
-    	adyacente(productoNocivo, verduras, L),
-    	adyacente(productoNocivo, carnes, L),
-    	adyacente(carnes, productoNocivo, L),
-    	adyacente(pescados, productoNocivo, L),
-    	adyacente(productoNocivo, pescados, L),
-    	sedes(S, D),
-	num_palets(L, N),
-	palets30(N),
-	distancia300(D),
-    	cam(N, D, X),
-    	append([], [X, D, N], I),
+general(Info, Productos, Sedes, Orden) :- permutation(Orden, Productos), planificador(Info, Orden, Sedes).
+
+
+planificador(Info, Productos, Sedes) :-
+    	productos(Productos),
+    	adyacente(productoNocivo, frutas, Productos),
+    	adyacente(frutas, productoNocivo, Productos),
+    	adyacente(verduras, productoNocivo, Productos),
+    	adyacente(productoNocivo, verduras, Productos),
+    	adyacente(productoNocivo, carnes, Productos),
+    	adyacente(carnes, productoNocivo, Productos),
+    	adyacente(pescados, productoNocivo, Productos),
+    	adyacente(productoNocivo, pescados, Productos),
+    	sedes(Sedes, Distancia),
+	num_palets(Productos, Palets),
+	palets30(Palets),
+	distancia300(Distancia),
+    	cam(Palets, Distancia, Camion),
+    	append([], [Camion, Distancia, Palets], Info),
     	write('\n El tipo de camión es:  '),
-    	display(X),
+    	display(Camion),
     	write('\n La distancia a recorrer es:  '),
-    	display(D),
+    	display(Distancia),
     	write('\n El número de palets es:  '),
-    	display(N).
+    	display(Palets).
